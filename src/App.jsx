d204560e267b27cb253cf784d592eb8efd539abb
Rohit Kumar Singh <93372627+rohitsingh-23@users.noreply.github.com>
2022-08-05 02:02:57 +0530

    stopwatch II

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState({ hr: 0, min: 0, sec: 0 });
 
   useEffect(() => {
+    // handelTime();
     return () => clearInterval(id.current);
   }, []);
 
@@ -12,14 +13,37 @@ function App() {
 
   function handelTime() {
     id.current = setInterval(() => {
-      setTime((prev) => prev + 1);
-      console.log(time);
+      setTime((prev) => {
+        if (prev.sec == 60) {
+          return { ...prev, min: prev.min + 1, sec: 0 };
+        }
+        if (prev.min == 60) {
+          return { ...prev, hr: prev.hr + 1, min: 0, sec: 0 };
+        }
+
+        return { ...prev, sec: prev.sec + 1 };
+      });
     }, 1000);
   }
 
   return (
     <div className="App">
-      <h1>{time}</h1>
+      <h1>
+        {time.hr.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+        :
+        {time.min.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+        :
+        {time.sec.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+      </h1>
       <button onClick={() => handelTime()}>Start</button>
       <button onClick={() => clearInterval(id.current)}>Pause</button>
       <button
